e1f3cad6e415b7a1d0d80e507a8be16270240d8f
SXC8286 <shawn_conrad@homedepot.com>
2015-07-01 13:04:44 -0400

    Details positioned

--- client/dead-viewer/partial/dead/dead.js
+++ client/dead-viewer/partial/dead/dead.js
@@ -12,6 +12,18 @@ angular.module('deadViewer').controller('DeadCtrl', function ($scope, socketServ
             "url": "http://www.twitch.tv/tsm_theoddone",
             "followers": 809389,
             "views": 183575215
+        },
+        {
+            "id": 15097482160,
+            "game": "League of Legends",
+            "viewers": 21889,
+            "preview": "http://static-cdn.jtvnw.net/previews-ttv/live_user_tsm_theoddone-400x300.jpg",
+            "displayName": "TSM_TheOddOne",
+            "logo": "http://static-cdn.jtvnw.net/jtv_user_pictures/tsm_theoddone-profile_image-338e7c68c54f0574-300x300.png",
+            "statusMessage": "TSM TheOddOne Ranked 5s Hype, FFXIV at 8PM PDT",
+            "url": "http://www.twitch.tv/tsm_theoddone",
+            "followers": 809389,
+            "views": 183575215
         }
     ];
     var timer = 0;
